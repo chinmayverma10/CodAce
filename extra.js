2aacdace493df81864eba1e61e238ca8b51bed05
Chinmay <chinmay.codesworld@gmail.com>
2024-04-01 14:55:15 +0530

    only commit

---
+++ extra.js
@@ -0,0 +1,10 @@
+
+
+// submitform.addEventListener('click',function(){
+//     alert('Thank you !! We have noted your query')
+// })
+
+
+function submitalert(){
+    alert('Your Query is successfully submited ');
+}
